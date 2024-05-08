39715ef32c3f0f32e8511e0214bba4ceae9f2dfa
Martin <22301061550038@cetis155.edu.mx>
2024-05-07 23:09:15 -0600

    queries solucionados en la parte de "%s" (debido a que cambie la conexion por mi modulo, hubo que adaptarla)

--- inicio.py
+++ inicio.py
@@ -36,7 +36,7 @@ def area(tabla):
 def area_editar(id):
     conexion=Admin()
     
-    conexion.execute('select idArea, descripcion from area where idArea = %s', (id))
+    conexion.execute('select idArea, descripcion from area where idArea = %s'%(id))
     dato  = conexion.getResult()
 
     return render_template("area_edi.html", comentar=dato[0])
@@ -48,7 +48,7 @@ def area_fedita(id):
         
         conexion=Admin()
         
-        conexion.execute('update area set descripcion=%s where idArea=%s', (desc,id))
+        conexion.execute('update area set descripcion=%s where idArea=%s'%(desc,id))
         
     return redirect(url_for('area'))
 
@@ -72,7 +72,7 @@ def area_fagrega():
         
         conexion=Admin()
         
-        conexion.execute('insert into area (descripcion) values (%s)',(desc))
+        conexion.execute('insert into area (descripcion) values (%s)'%(desc))
         
     return redirect(url_for('area'))
 
@@ -100,32 +100,31 @@ def puesto_fdetalle(idP):
     conexion.execute('select idPuesto, nomPuesto from puesto order by idPuesto')
     datos = conexion.getResult()
 
-    conexion.execute('select idPuesto,codPuesto,idArea,nomPuesto,puestoJefeSup,jornada,remunMensual,prestaciones,descripcionGeneral,'
-            'funciones,edad,sexo,idEstadoCivil,idEscolaridad,idGradoAvance,idCarrera,experiencia,conocimientos,manejoEquipo,'
-            'reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo from puesto where idPuesto = %s', (idP))
+    conexion.execute(f"""select idPuesto,codPuesto,idArea,nomPuesto,puestoJefeSup,jornada,remunMensual,prestaciones,descripcionGeneral,
+            funciones,edad,sexo,idEstadoCivil,idEscolaridad,idGradoAvance,idCarrera,experiencia,conocimientos,manejoEquipo,'
+            'reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo from puesto where idPuesto = {idP}""" )
     dato = conexion.getResult()
 
-    conexion.execute('select a.idArea, a.descripcion from area a, puesto b where a.idArea = b.idArea and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idArea, a.descripcion from area a, puesto b where a.idArea = b.idArea and b.idPuesto = %s'%(idP))
     datos1 = conexion.getResult()
 
-    conexion.execute('select a.idEstadoCivil, a.descripcion from estado_civil a, puesto b where a.idEstadoCivil = b.idEstadoCivil and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idEstadoCivil, a.descripcion from estado_civil a, puesto b where a.idEstadoCivil = b.idEstadoCivil and b.idPuesto = %s'%(idP))
     datos2 = conexion.getResult()
 
-    conexion.execute('select a.idEscolaridad, a.descripcion from escolaridad a, puesto b where a.idEscolaridad = b.idEscolaridad and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idEscolaridad, a.descripcion from escolaridad a, puesto b where a.idEscolaridad = b.idEscolaridad and b.idPuesto = %s'%(idP))
     datos3 = conexion.getResult()
 
-    conexion.execute('select a.idGradoAvance, a.descripcion from grado_avance a, puesto b where a.idGradoAvance = b.idGradoAvance and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idGradoAvance, a.descripcion from grado_avance a, puesto b where a.idGradoAvance = b.idGradoAvance and b.idPuesto = %s'%(idP))
     datos4 = conexion.getResult()
 
-    conexion.execute('select a.idCarrera, a.descripcion from carrera a, puesto b where a.idCarrera = b.idCarrera and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idCarrera, a.descripcion from carrera a, puesto b where a.idCarrera = b.idCarrera and b.idPuesto = %s'%(idP))
     datos5 = conexion.getResult()
 
     conexion.execute('select a.idPuesto, b.idIdioma, b.descripcion from puesto a, idioma b, puesto_has_idioma c '
-                   'where a.idPuesto = c.idPuesto and b.idIdioma = c.idIdioma and a.idPuesto = %s', (idP))
+                   'where a.idPuesto = c.idPuesto and b.idIdioma = c.idIdioma and a.idPuesto = %s'%(idP))
     datos6 = conexion.getResult()
 
-    conexion.execute('select a.idPuesto, b.idHabilidad, b.descripcion from puesto a, habilidad b, puesto_has_habilidad c '
-                   'where a.idPuesto = c.idPuesto and b.idHabilidad = c.idHabilidad and a.idPuesto = %s', (idP))
+    conexion.execute(f'select a.idPuesto, b.idHabilidad, b.descripcion from puesto a, habilidad b, puesto_has_habilidad c where a.idPuesto = c.idPuesto and b.idHabilidad = c.idHabilidad and a.idPuesto = {idP}')
     datos7 = conexion.getResult()
     return render_template("puesto.html", pue = datos, dat=dato[0], catArea=datos1[0], catEdoCivil=datos2[0], catEscolaridad=datos3[0],
                            catGradoAvance=datos4[0], catCarrera=datos5[0], catIdioma=datos6, catHabilidad=datos7)
@@ -135,11 +134,11 @@ def puesto_borrar(idP):
     
     conexion=Admin()
     
-    conexion.execute('delete from puesto where idPuesto = %s',(idP))
+    conexion.execute('delete from puesto where idPuesto = %s'%(idP))
     
-    conexion.execute('delete from puesto_has_habilidad where idPuesto =%s ', (idP))
+    conexion.execute('delete from puesto_has_habilidad where idPuesto =%s '%(idP))
     
-    conexion.execute('delete from puesto_has_idioma where idPuesto =%s ', (idP))
+    conexion.execute('delete from puesto_has_idioma where idPuesto =%s '%(idP))
     
     return redirect(url_for('puesto'))
 
@@ -224,9 +223,9 @@ def puesto_fagrega():
     conexion=Admin()
     
     conexion.execute(
-    'insert into puesto (codPuesto,idArea,nomPuesto,puestoJefeSup,jornada,remunMensual,prestaciones,descripcionGeneral,'
-    'funciones,edad,sexo,idEstadoCivil,idEscolaridad,idGradoAvance,idCarrera,experiencia,conocimientos,manejoEquipo,'
-    'reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+    """insert into puesto (codPuesto,idArea,nomPuesto,puestoJefeSup,jornada,remunMensual,prestaciones,descripcionGeneral,
+    funciones,edad,sexo,idEstadoCivil,idEscolaridad,idGradoAvance,idCarrera,experiencia,conocimientos,manejoEquipo,
+    reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""%
     (codP, idAr, nomP, pueJ, jorn, remu, pres, desc, func, eda, sex, idEC, idEs, idGA, idCa, expe, cono, manE, reqF,
      reqP, resp, conT))
     
@@ -244,7 +243,7 @@ def puesto_fagrega():
     for i in range(1, ni):
         idio = 'i' + str(i)
         if idio in request.form:
-            conexion.execute('insert into puesto_has_idioma(idPuesto,idIdioma) values (%s,%s)', (idP, i))
+            conexion.execute('insert into puesto_has_idioma(idPuesto,idIdioma) values (%s,%s)'%(idP, i))
             
 
     conexion.execute('select count(*) from habilidad ')
@@ -255,7 +254,7 @@ def puesto_fagrega():
     for i in range(1,nh):
         habi = 'h' + str(i)
         if habi in request.form:
-            conexion.execute('insert into puesto_has_habilidad(idPuesto,idHabilidad) values (%s,%s)', (idP,i))
+            conexion.execute('insert into puesto_has_habilidad(idPuesto,idHabilidad) values (%s,%s)'%(idP,i))
             
 
     return redirect(url_for('puesto'))
@@ -270,7 +269,7 @@ def puesto_editar(idP):
 
     conexion.execute('select idPuesto,codPuesto,idArea,nomPuesto,puestoJefeSup,jornada,remunMensual,prestaciones,descripcionGeneral,'
         'funciones,edad,sexo,idEstadoCivil,idEscolaridad,idGradoAvance,idCarrera,experiencia,conocimientos,manejoEquipo,'
-        'reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo from puesto where idPuesto = %s', (idP))
+        'reqFisicos,reqPsicologicos,responsabilidades,condicionesTrabajo from puesto where idPuesto = %s'%(idP))
     dato = conexion.getResult()
 
     conexion.execute('select idArea, descripcion from area ')
@@ -294,27 +293,27 @@ def puesto_editar(idP):
     conexion.execute('select idHabilidad, descripcion from habilidad ')
     datos7 = conexion.getResult()
 
-    conexion.execute('select a.idArea, a.descripcion from area a, puesto b where a.idArea = b.idArea and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idArea, a.descripcion from area a, puesto b where a.idArea = b.idArea and b.idPuesto = %s'%(idP))
     datos11 = conexion.getResult()
 
-    conexion.execute('select a.idEstadoCivil, a.descripcion from estado_civil a, puesto b where a.idEstadoCivil = b.idEstadoCivil and b.idPuesto = %s',(idP))
+    conexion.execute('select a.idEstadoCivil, a.descripcion from estado_civil a, puesto b where a.idEstadoCivil = b.idEstadoCivil and b.idPuesto = %s'%(idP))
     datos12 = conexion.getResult()
 
-    conexion.execute('select a.idEscolaridad, a.descripcion from escolaridad a, puesto b where a.idEscolaridad = b.idEscolaridad and b.idPuesto = %s',(idP))
+    conexion.execute('select a.idEscolaridad, a.descripcion from escolaridad a, puesto b where a.idEscolaridad = b.idEscolaridad and b.idPuesto = %s'%(idP))
     datos13 = conexion.getResult()
 
-    conexion.execute('select a.idGradoAvance, a.descripcion from grado_avance a, puesto b where a.idGradoAvance = b.idGradoAvance and b.idPuesto = %s',(idP))
+    conexion.execute('select a.idGradoAvance, a.descripcion from grado_avance a, puesto b where a.idGradoAvance = b.idGradoAvance and b.idPuesto = %s'%(idP))
     datos14 = conexion.getResult()
 
-    conexion.execute('select a.idCarrera, a.descripcion from carrera a, puesto b where a.idCarrera = b.idCarrera and b.idPuesto = %s', (idP))
+    conexion.execute('select a.idCarrera, a.descripcion from carrera a, puesto b where a.idCarrera = b.idCarrera and b.idPuesto = %s'%(idP))
     datos15 = conexion.getResult()
 
     conexion.execute('select a.idPuesto, b.idIdioma, b.descripcion from puesto a, idioma b, puesto_has_idioma c '
-                   'where a.idPuesto = c.idPuesto and b.idIdioma = c.idIdioma and a.idPuesto = %s', (idP))
+                   'where a.idPuesto = c.idPuesto and b.idIdioma = c.idIdioma and a.idPuesto = %s'%(idP))
     datos16 = conexion.getResult()
 
     conexion.execute('select a.idPuesto, b.idHabilidad, b.descripcion from puesto a, habilidad b, puesto_has_habilidad c '
-                   'where a.idPuesto = c.idPuesto and b.idHabilidad = c.idHabilidad and a.idPuesto = %s', (idP))
+                   'where a.idPuesto = c.idPuesto and b.idHabilidad = c.idHabilidad and a.idPuesto = %s'%(idP))
     datos17 = conexion.getResult()
 
 
@@ -354,17 +353,17 @@ def puesto_fedita(idP):
     conexion=Admin()
 
 
-    conexion.execute('update puesto set codPuesto = %s, idArea = %s, nomPuesto = %s, puestoJefeSup = %s, jornada = %s, '
-                   'remunMensual = %s, prestaciones = %s, descripcionGeneral = %s, funciones = %s, edad = %s, sexo = %s, '
-                   'idEstadoCivil = %s, idEscolaridad = %s, idGradoAvance = %s, idCarrera = %s, experiencia = %s, '
-                   'conocimientos = %s, manejoEquipo = %s, reqFisicos = %s, reqPsicologicos = %s, responsabilidades = %s, '
-                   'condicionesTrabajo = %s where idPuesto = %s', (codP, idAr, nomP, pueJ, jorn, remu, pres, desc, func, eda,
+    conexion.execute("""update puesto set codPuesto = %s, idArea = %s, nomPuesto = %s, puestoJefeSup = %s, jornada = %s,
+                  remunMensual = %s, prestaciones = %s, descripcionGeneral = %s, funciones = %s, edad = %s, sexo = %s,
+                  idEstadoCivil = %s, idEscolaridad = %s, idGradoAvance = %s, idCarrera = %s, experiencia = %s,
+                  conocimientos = %s, manejoEquipo = %s, reqFisicos = %s, reqPsicologicos = %s, responsabilidades = %s,
+                  condicionesTrabajo = %s where idPuesto = %s"""%(codP, idAr, nomP, pueJ, jorn, remu, pres, desc, func, eda,
                    sex, idEC, idEs, idGA, idCa, expe, cono, manE, reqF, reqP, resp, conT, idP))
     
 
-    conexion.execute('delete from puesto_has_habilidad where idPuesto =%s ', (idP))
+    conexion.execute('delete from puesto_has_habilidad where idPuesto =%s '%(idP))
     
-    conexion.execute('delete from puesto_has_idioma where idPuesto =%s ', (idP))
+    conexion.execute('delete from puesto_has_idioma where idPuesto =%s '%(idP))
     
 
     conexion.execute('select count(*) from idioma ')
@@ -375,7 +374,7 @@ def puesto_fedita(idP):
     for i in range(1, ni):
         idio = 'i' + str(i)
         if idio in request.form:
-            conexion.execute('insert into puesto_has_idioma(idPuesto,idIdioma) values (%s,%s)', (idP, i))
+            conexion.execute('insert into puesto_has_idioma(idPuesto,idIdioma) values (%s,%s)'%(idP, i))
             
 
     conexion.execute('select count(*) from habilidad ')
@@ -386,7 +385,7 @@ def puesto_fedita(idP):
     for i in range(1, nh):
         habi = 'h' + str(i)
         if habi in request.form:
-            conexion.execute('insert into puesto_has_habilidad(idPuesto,idHabilidad) values (%s,%s)', (idP, i))
+            conexion.execute('insert into puesto_has_habilidad(idPuesto,idHabilidad) values (%s,%s)'%(idP, i))
             
     return redirect(url_for('puesto'))
 
