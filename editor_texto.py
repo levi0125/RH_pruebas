bd5d8e959935d65ae9681348414cbff6ef57b8d5
Martin <martnalej@gmail.com>
2024-05-08 07:54:57 -0600

    conversores de titulo, nombre de tabla y id

--- editor_texto.py
+++ editor_texto.py
@@ -29,3 +29,34 @@ class Editor:
             return True
         except Exception:
             return False
+        
+    def tablaToTitle(self,tabla):
+        tablas=["area","carrera","escolaridad","estado_civil","grado_avance","habilidad","idioma"]
+        titulos=["Area","Carrera","Escolaridad","Estado Civil","Grado de Avance","Habilidad","Idioma"]
+
+        try:
+            c=tablas.index(tabla)
+            return titulos[c]
+        except Exception:
+            return None
+        
+    def titleToTabla(self,titulo):
+        tablas=["area","carrera","escolaridad","estado_civil","grado_avance","habilidad","idioma"]
+        titulos=["Area","Carrera","Escolaridad","Estado Civil","Grado de Avance","Habilidad","Idioma"]
+
+        try:
+            c=titulos.index(titulo)
+            return tablas[c]
+        except Exception:
+            return None
+    
+    def tablaId(self,nombre_tabla):
+        tablas=["area","carrera","escolaridad","estado_civil","grado_avance","habilidad","idioma"]
+        ids=["idArea","idCarrera","idEscolaridad","idEstadoCivil","idGradoAvance","idHabilidad","idIdioma"]
+
+        try:
+            c=tablas.index(nombre_tabla)
+            return ids[c]
+        except Exception:
+            return None
+        
